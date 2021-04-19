333996283b36b7122d7f85a4f87cb838249d7d83
Simon <a20simst@student.his.se>
2021-04-19 09:57:00 +0200

    Added intent variabel

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         knapp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this,  OnClickActivity.class);
+                startActivity(intent);
             }
         });
     }
