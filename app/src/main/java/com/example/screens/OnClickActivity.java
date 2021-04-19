94d6eacd721e064c0fbb10854976d2dd2a46a70c
Simon <a20simst@student.his.se>
2021-04-19 10:08:06 +0200

    Added id to close button. Added a finnish function to close button. On click the activity closes.

--- app/src/main/java/com/example/screens/OnClickActivity.java
+++ app/src/main/java/com/example/screens/OnClickActivity.java
@@ -3,6 +3,8 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OnClickActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class OnClickActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_click);
+        Button closeActivity = findViewById(R.id.close);
+        closeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
